599ffb2be4ce5add9d9b82dce0a31ffa1d47ff23
DISNOTACAY <yeonjh915@gmail.com>
2025-01-06 04:09:59 +0900

    Refactor: 컨트롤러에서 사용하지 않는 @Slf4j 어노테이션 삭제

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/presentation/rest/controller/InventoryCommandController.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/presentation/rest/controller/InventoryCommandController.java
@@ -6,7 +6,6 @@ import com.bkmarriott.reservationservice.reservation.presentation.rest.util.repo
 import com.bkmarriott.reservationservice.reservation.presentation.rest.util.reponse.ApiResponse.Success;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/reservations/inventories")
