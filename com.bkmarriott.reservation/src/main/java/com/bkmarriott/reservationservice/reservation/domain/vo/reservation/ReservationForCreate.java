2267ead32326293351a5d3ec8b85d4ad88094be9
DISNOTACAY <yeonjh915@gmail.com>
2025-01-09 14:56:05 +0900

    Feat: 예약 생성 dto 수정

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/domain/vo/reservation/ReservationForCreate.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/domain/vo/reservation/ReservationForCreate.java
@@ -15,18 +15,28 @@ public class ReservationForCreate {
   private LocalDate endDate;
   private RoomType roomType;
   private ReservationStatus status;
+  private Long memberCouponId;
+  private int originPrice;
+  private int finalPrice;
 
   public ReservationForCreate(Long userId, Long hotelId, LocalDate startDate,
-      LocalDate endDate, RoomType roomType) {
+      LocalDate endDate, RoomType roomType, Long memberCouponId, int price) {
     this.userId = userId;
     this.hotelId = hotelId;
     this.startDate = startDate;
     this.endDate = endDate;
     this.roomType = roomType;
     this.status = ReservationStatus.PENDING;
+    this.memberCouponId = memberCouponId;
+    this.originPrice = price;
+    this.finalPrice = price;
   }
 
   public void setPaid() {
     this.status = ReservationStatus.PAID;
+  }
+
+  public void discoundOriginPriceByCoupon(Float discountRate) {
+    this.finalPrice = Math.round(originPrice * (1 - discountRate));
   }
 }
