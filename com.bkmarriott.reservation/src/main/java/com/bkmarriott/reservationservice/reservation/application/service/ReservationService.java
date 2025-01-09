589367b7e04909625d0d177810987e35d248d0a2
DISNOTACAY <yeonjh915@gmail.com>
2025-01-09 14:39:00 +0900

    Feat: createReservation 기능 구현 - 쿠폰 검증과 사용을 동시에 요청하며 할인율을 반환받습니다. - payment-service 를 통해 결제 정보를 저장합니다. (reservationId 는 null 로 저장됩니다.) - 예약 저장 후 예약 객실 수를 증가 처리, 결제 정보에 reservationId를 추가 합니다.

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/application/service/ReservationService.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/application/service/ReservationService.java
@@ -1,12 +1,12 @@
 package com.bkmarriott.reservationservice.reservation.application.service;
 
-import com.bkmarriott.reservationservice.reservation.application.dto.inventory.InventoryQueryResponseDto;
-import com.bkmarriott.reservationservice.reservation.application.exception.reservation.NoRoomAvailable;
 import com.bkmarriott.reservationservice.reservation.application.exception.reservation.RoomSaveFailure;
+import com.bkmarriott.reservationservice.reservation.application.outputport.CouponOutputPort;
+import com.bkmarriott.reservationservice.reservation.application.outputport.PaymentOutputPort;
 import com.bkmarriott.reservationservice.reservation.application.outputport.reservation.ReservationCommandOutputPort;
 import com.bkmarriott.reservationservice.reservation.domain.Reservation;
 import com.bkmarriott.reservationservice.reservation.domain.vo.reservation.ReservationForCreate;
-import com.bkmarriott.reservationservice.reservation.domain.vo.reservation.ReservationStatus;
+import com.bkmarriott.reservationservice.reservation.infrastructure.feignClient.dto.payment.CreatePayment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,41 +19,44 @@ public class ReservationService {
 
   private final ReservationCommandOutputPort reservationCommandOutputPort;
   private final InventoryService inventoryService;
+  private final CouponOutputPort couponOutputPort;
+  private final PaymentOutputPort paymentOutputPort;
 
   @Transactional
   public Reservation createReservation(ReservationForCreate reservationForCreate) {
 
     // 해당 객실의 여유분 확인
-    InventoryQueryResponseDto availableRoom = inventoryService.getInventoryQuantityByRoomType(
+    int availableRoom = inventoryService.findAvailableRoomsWithPessimisticLock(
         reservationForCreate.getHotelId(),
         reservationForCreate.getStartDate(),
         reservationForCreate.getEndDate(),
         reservationForCreate.getRoomType()
     );
-    log.info("[ReservationService] [createReservation] availableRoom ::: {}", availableRoom.getQuantity());
+    log.info("[ReservationService] [createReservation] availableRoom ::: {}", availableRoom);
 
+    // 쿠폰 할인가 적용
+    if(reservationForCreate.getMemberCouponId() != null) {
+      Float discountRate = couponOutputPort.getCouponDiscountRate(reservationForCreate.getMemberCouponId());
+      log.info("[ReservationService] [GetMemberCouponResponse] coupon discountRate ::: {}", discountRate);
+      reservationForCreate.discoundOriginPriceByCoupon(discountRate);
 
-    if(availableRoom.getQuantity() == 0) {
-      // 동시성 테스트 필요 (현재 비관적락 적용)
-      throw new NoRoomAvailable("예약가능한 잔여 객실이 없습니다.");
+      // TODO: Feign 쿠폰 사용 요청
     }
-    // Feign 쿠폰 사용 요청
 
-    // Feign 결제 요청
+    // 결제 정보 저장
+    Long paymentId = paymentOutputPort.createPayment(CreatePayment.Requset.from(reservationForCreate));
+    log.info("[ReservationService] [createPayment] paymentId ::: {}", paymentId);
 
-    // 결제 응답 후, 객실 인벤토리 업데이트 요청
-    // TODO: payment-service 생성 후 변경 필요
-    ReservationStatus paymentResponse = ReservationStatus.PAID;
-    if(paymentResponse == ReservationStatus.PAID) {
-      reservationForCreate.setPaid();
-      inventoryService.increasePaidReserved(reservationForCreate);
-    }
+    // 결제 후 객실 잔여 수량 수정
+    reservationForCreate.setPaid();
+    inventoryService.increasePaidReserved(reservationForCreate);
 
     // 예약 응답
     try {
       Reservation reservation = reservationCommandOutputPort.save(
           reservationForCreate);
       log.info("[ReservationService] [createReservation] reservationId ::: {}", reservation.getReservationId());
+      paymentOutputPort.updateReservationId(paymentId, reservation.getReservationId());
       return reservation;
     } catch (Exception e) {
       throw new RoomSaveFailure("객실 예약에 실패하였습니다.");
