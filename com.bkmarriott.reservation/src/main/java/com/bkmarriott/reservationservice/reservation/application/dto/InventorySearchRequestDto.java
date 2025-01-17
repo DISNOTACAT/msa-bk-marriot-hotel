c3adec27d0cc4a76de741e4cedd5a6eb0d1f5fde
DISNOTACAT <yeonjh915@gmail.com>
2025-01-17 16:09:27 +0900

    fix: 모호한 이름의 Dto 수정 및 roomCount 필드명 통일 - quantity -> roomCount 로 통일화 - request, response 를 record 객체로 수정

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/application/dto/InventorySearchRequestDto.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/application/dto/InventorySearchRequestDto.java
@@ -1,19 +1,19 @@
 package com.bkmarriott.reservationservice.reservation.application.dto;
 
 import java.time.LocalDate;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class InventoryQueryRequestDto {
-
+public class InventorySearchRequestDto{
   private Long hotelId;
   private LocalDate startDate;
   private LocalDate endDate;
 
-  public InventoryQueryRequestDto(Long hotelId, LocalDate startDate, LocalDate endDate) {
+  @Builder
+  public InventorySearchRequestDto(Long hotelId, LocalDate startDate, LocalDate endDate) {
     this.hotelId = hotelId;
     this.startDate = startDate;
     this.endDate = endDate;
   }
-
 }
