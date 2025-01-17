19de18f6c6ddecaac84c9bd888ec394e04ec9e9e
DISNOTACAT <yeonjh915@gmail.com>
2025-01-17 16:20:22 +0900

    fix: dto 변경에 따른 파라미터 수정 - 여러개의 인자를 받는 메서드를 dto 객체로 수정

--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/application/outputport/feign/ChargeOutputPort.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/application/outputport/feign/ChargeOutputPort.java
@@ -1,9 +1,8 @@
 package com.bkmarriott.reservationservice.reservation.application.outputport.feign;
 
-import java.time.LocalDate;
-import com.bkmarriott.reservationservice.reservation.domain.vo.RoomType;
+import com.bkmarriott.reservationservice.reservation.domain.vo.InventoryQuery;
 
 
 public interface ChargeOutputPort {
-    int getRoomCharge(Long hotelId, RoomType roomType, LocalDate startDate, LocalDate endDate);
+    int getRoomCharge(InventoryQuery query);
 }
--- com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/infrastructure/feignClient/adapter/ChargeFeignClientAdapter.java
+++ com.bkmarriott.reservation/src/main/java/com/bkmarriott/reservationservice/reservation/infrastructure/feignClient/adapter/ChargeFeignClientAdapter.java
@@ -1,9 +1,8 @@
 package com.bkmarriott.reservationservice.reservation.infrastructure.feignClient.adapter;
 
 import com.bkmarriott.reservationservice.reservation.application.outputport.feign.ChargeOutputPort;
-import com.bkmarriott.reservationservice.reservation.domain.vo.RoomType;
+import com.bkmarriott.reservationservice.reservation.domain.vo.InventoryQuery;
 import com.bkmarriott.reservationservice.reservation.infrastructure.feignClient.client.ChargeClient;
-import java.time.LocalDate;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,11 +14,11 @@ public class ChargeFeignClientAdapter implements ChargeOutputPort {
     private final ChargeClient chargeClient;
 
     @Override
-    public int getRoomCharge(Long hotelId, RoomType roomType, LocalDate startDate, LocalDate endDate) {
+    public int getRoomCharge(InventoryQuery query) {
 
-        return Stream.iterate(startDate, date -> date.plusDays(1))
-            .limit(startDate.until(endDate).getDays() + 1)
-            .mapToInt(date -> chargeClient.getRoomCharge(hotelId, roomType, date).charge())
+        return Stream.iterate(query.startDate(), date -> date.plusDays(1))
+            .limit(query.startDate().until(query.endDate()).getDays() + 1)
+            .mapToInt(date -> chargeClient.getRoomCharge(query.hotelId(), query.roomType(), date).charge())
             .sum();
     }
 
